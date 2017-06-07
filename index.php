f8cf50f97399fe35c08cd13089d6b2da9455f9d3
s15007 <s15007@std.it-college.ac.jp>
2017-06-07 14:01:56 +0900

    修正

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="ja">
 <head>
+    <meta charset="UTF-8">
+    <title>PBL2017</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 <?php require_once 'header.php'; ?>
-
 
 </body>
 </html>
