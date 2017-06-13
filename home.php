39407fa4950256cf4bbd98c11e299bf38edcb829
Samuel Kota Nakachi <s15007@std.it-college.ac.jp>
2017-06-13 17:31:55 +0900

    Update home.php

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<link rel="stylesheet" href="home.css" type="text/css">
+</head>
+<meta charset="UTF-8">
+	<title>PBL</title>
+<body>
+
+
+<main class="clearfix">
+	<div class="header">
+		<img src="images/nae.png">
+		<h1>注文した苗一覧</h1>
+	</div>
+
+
+<div class="colum">
+	<img src="images/tomato.png">
+	<div class="details">
+
+	<h1>トマト</h1>
+
+
+    <div class="dcol">
+     	<h3>トレイ規格：<span>200穴</span></h3>
+        <h3 id="right">育苗方法：<span>自根</span></h3>
+        <h3>台木：<span>なし</span></h3>
+        </div>
+
+        <div class="dcol2">
+         <!--<h3>台木：<span>なし</span></h3>-->
+         <h3 id="right">注文数：<span>500</span></h3>
+                <h3>合計：<span>3000円</span></h3>
+    </div>
+        </div>
+</div>
+</main>
+</body>
+</html>
