be12abb6a9b4f11315c1c2f0b052c32b6c809681
Samuel Kota Nakachi <s15007@std.it-college.ac.jp>
2017-06-13 17:31:12 +0900

    Create login.php

--- login.php
+++ login.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<link rel="stylesheet" href="login.css" type="text/css">
+<link rel="stylesheet" href="css/login.css" type="text/css">
 </head>
 <meta charset="UTF-8">
 <title>login</title>
 <body>
 	<header>
-	<h1>苗ファクトリーOnline</h1>
+	<h1>
+	<img src="images/nae.png" width="35">
+	苗ファクトリーOnline
+	</h1>
 	</header>
 
 
@@ -17,20 +20,29 @@
 	１回の発注で１種類最大１万まで注文可能<br>
 	苗ファクトリーの会員様限定オンライン発注サイト</h2>
 	</p>
-
+<!--ログインフォーム-->
     <form >
+
     <p>
     <input type="text" placeholder="メールアドレスを入力してください"/>
     </p>
-   <p>
-    <input type="text" placeholder="パスワードを入力してください" />
-    </p>
-    
-    <a href="#" class="sample">ログイン</a>
-    </form>
 
-   
+   <p>
+    <input type="password" placeholder="パスワードを入力してください" />
+    </p>
+
+    	<ul>
+     <input type="submit" value="ログイン" >
+   	</ul>
+     </form>
+  <!--ここまでログインフォーム-->
 </div>
+
+
+<!--背景画像-->
+<img src="images/wood.gif" width="500" style="position:absolute;bottom:0px;right:0px;">
+<img src="images/wood2.gif" width="350" style="position: absolute;bottom:0px;left:0px;">
+<!--ここまで背景画像-->
 
 		</body>
 </html>
