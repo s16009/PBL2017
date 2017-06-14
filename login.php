3d9838aa715abdb31953f43c47ad971ce108fb97
s15007 <s15007@std.it-college.ac.jp>
2017-06-14 15:17:16 +0900

    修正

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if (isset($_POST["login"])) {
             if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 if ($_POST['pass']==$row['password'] && $_POST['mail']==$row['mail_address']) {
                     session_regenerate_id(true);
-                  
+
                     header("Location: home.php");  // メイン画面へ遷移
                     exit();  // 処理終了
                 } else {
@@ -53,36 +53,39 @@ if (isset($_POST["login"])) {
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<link rel="stylesheet" href="login.css" type="text/css">
+    <link rel="stylesheet" href="css/login.css" type="text/css">
 </head>
 <meta charset="UTF-8">
 <title>login</title>
 <body>
-	<header>
-	<h1>苗ファクトリーOnline</h1>
-	</header>
+<header>
+    <h1>苗ファクトリーOnline</h1>
+</header>
 
 
 <div class="login">
-	<p>
-	<h2>業界トップクラスの品揃えと激安価格<br>
-	１回の発注で１種類最大１万まで注文可能<br>
-	苗ファクトリーの会員様限定オンライン発注サイト</h2>
-	</p>
+    <p>
+    <h2>業界トップクラスの品揃えと激安価格<br>
+        １回の発注で１種類最大１万まで注文可能<br>
+        苗ファクトリーの会員様限定オンライン発注サイト</h2>
+    </p>
 
     <form action="" method="POST">
-    <p>
-    <input type="mail" name="mail" placeholder="メールアドレスを入力してください"/>
-    </p>
-   <p>
-    <input type="password" name="pass" placeholder="パスワードを入力してください" />
-    </p>
-    
-    <input type="submit" name="login" value="ログイン">
+        <p>
+            <input type="email" name="mail" placeholder="メールアドレスを入力してください"/>
+        </p>
+        <p>
+            <input type="password" name="pass" placeholder="パスワードを入力してください" />
+        </p>
+
+        <input type="submit" name="login" value="ログイン">
     </form>
 
-   <?php echo $errorMessage; ?>
+    <?php echo $errorMessage; ?>
+
+    <img src="images/wood.gif" width="500" style="position:absolute;bottom:0px;right:0px;">
+    <img src="images/wood2.gif" width="350" style="position: absolute;bottom:0px;left:0px;">
 </div>
 
-		</body>
+</body>
 </html>
