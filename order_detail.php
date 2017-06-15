e018c2819bb791bad7025882063e3ac0895ec9cc
s15007 <s15007@std.it-college.ac.jp>
2017-06-16 08:51:55 +0900

    修正

--- order_detail.php
+++ order_detail.php
@@ -6,55 +6,70 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/order_detail.css">
 </head>
 <body>
 <?php require_once 'header.php'; ?>
+<?php require_once 'functions/php_functions.php'; ?>
+
+<?php
+$productName = "きゅうり";
+
+switch ($productName) {
+    case "トマト":
+        $checkProduct = "checkTomato()";
+        break;
+    case "きゅうり":
+        $checkProduct = "checkCucumber()";
+        break;
+    case "レタス":
+        $checkProduct = "checkLettuce()";
+        break;
+}
+
+?>
+<script>
+    var checkproduct = new CheckProduct();
+</script>
 
 <main>
-    <h1>レタス</h1>
+    <h1 id="productName"><?php echo $productName; ?></h1>
     <img src="images/tomato.png">
-    <form method="post" action="order_check.php">
+    <form method="post" action="order_check.php" id="orderDetail" name="orderDetail">
         <div class="colum form-group">
             <h2>育苗方法</h2>
-            <select class="form-control" id="sel1">
+            <select class="form-control" id="sel1" name="howToGrow" onchange="<?php echo "checkproduct.".$checkProduct; ?>">
+                <option value='' disabled selected style='display:none;'>育苗方法を選択</option>
                 <option>自根</option>
                 <option>接木</option>
             </select>
         </div>
         <div class="colum">
             <h2>トレイ規格</h2>
-            <select class="form-control" id="sel1">
-                <option>280穴</option>
-                <option>128穴</option>
-                <option>9cmポット</option>
-                <option>10.5cmポット</option>
+            <select class="form-control" id="sel1" name="trayName" onchange="<?php echo "checkproduct.".$checkProduct; ?>">
+                <option value='' disabled selected style='display:none;'>トレイ規格を選択</option>
+                <?php get_tray(); ?>
             </select>
         </div>
         <div class="colum">
             <h2>引き取り日</h2>
-            <input type="date" value="2017-06-07">
+            <input type="date" id="date" onchange="checkInput()">
         </div>
         <div class="colum">
             <h2>苗数量</h2>
-            <select class="form-control" id="sel1">
-                <?php for ($i = 0; $i < 10001; $i++)
-                    echo "<option>{$i}</option>";?>
-            </select>
+            <input type="number" min="1" id="p_num" name="product_number" class="form-control" max="10000" placeholder="苗数量を入力" onchange="checkInput()">
         </div>
         <div class="colum buttons">
-            <input id="button" type="submit" value="決定" />
+            <input id="button" type="submit" onmouseover="submitMouseover()" onmouseout="submitMouseout()" value="決定"/>
             <input id="button" type="button" onclick="location.href='SeedlingsList.php'" value="戻る" />
         </div>
         <div class="colum">
             <h2>台木</h2>
-            <select class="form-control" id="sel1">
-                <option>なし</option>
-                <option>きゅうり台木1</option>
-                <option>きゅうり台木2</option>
-                <option>トマト台木1</option>
-                <option>トマト台木2</option>
+            <select class="form-control" id="sel1" name="selectGraft" onchange="<?php echo "checkproduct.".$checkProduct; ?>">
+                <option value='' disabled selected style='display:none;'>台木を選択</option>
+                <?php get_rootstock(); ?>
             </select>
         </div>
     </form>
